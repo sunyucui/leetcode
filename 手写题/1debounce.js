4496226d8560439455078d6d8eedb8bc099eab8a
sunyucui <yucui.sun@weimob.com>
2023-09-21 11:44:58 +0800

    perf：防抖节流的思考

--- 手写题/1debounce.js
+++ 手写题/1debounce.js
@@ -1,6 +1,6 @@
 /**
  * 防抖
- * 闭包 计时器：没到时间刷新计时器
+ * 闭包 计时器：没到时间刷新计时器 用户全部活动结束后才开始执行 且执行一次
  */
 const debounce = function (fn, delay) {
   let timer = null;
@@ -11,3 +11,8 @@ const debounce = function (fn, delay) {
     timer = setTimeout(()=>{fn.apply(this, arguments)},delay)
   }
 }
+const fn = debounce(function(){
+  console.log('hi')
+}, 1000
+)
+document.getElementById('btn').addEventListener('click',fn)
--- 手写题/2throttle.js
+++ 手写题/2throttle.js
@@ -1,15 +1,26 @@
 /**
  * 节流
- * 闭包 定时器：一到时间立即执行
+ * 闭包 定时器：一到时间立即执行 立即就有结果 执行一次的结果
  * ? 为什么开始的时候 timer没有赋值 对比debounce开始的时候就赋值了？
  */
 const throttle = function (fn, delay) {
   let timer;
   return function () {
-    if(timer) return;
+    if(timer) return; //执行过了 就不再执行
     timer = setTimeout(() => {
       fn.apply(this, arguments);
       timer = null;
     },delay)
   }
 }
+const fn = throttle(function(){
+  console.log('hi')
+}, 5000
+)
+document.getElementById('btn').addEventListener('click',fn)
+
+/**
+ * 反思 防抖和节流 都是多次任务执行一次 
+ * 但是防抖是 等全部触发结束后 执行
+ * 节流是 时间一到就立即执行 延迟执行
+ */
