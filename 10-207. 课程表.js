14ccb090f6bb9399df7d60e016418931bc539fe7
sunyucui <yucui.sun@weimob.com>
2023-09-25 14:58:40 +0800

    每日5道

---
+++ 10-207. 课程表.js
@@ -0,0 +1,38 @@
+var canFinish = function(numCourses, prerequisites) {
+  const inDegree = new Array(numCourses).fill(0); //入度数组
+  const map = {}; //邻接表 key value:key的后续课 {0:[],1:[],2:[],,,}
+
+  for(let i=0;i < prerequisites.length; i++){
+    inDegree[prerequisites[i][0]]++; //有先修课的 入度+1
+    if(map[prerequisites[i][1]]) {
+      map[prerequisites[i][1]].push(prerequisites[i][0]);
+    }else {
+      map[prerequisites[i][1]] = [prerequisites[i][0]];
+    }
+  }
+
+  let queue = []; //记录所有没有先前课的 即入度为0；
+  for(let i=0;i<inDegree.length;i++) {
+    if(inDegree[i] === 0) queue.push(i);
+  }
+
+  let count = 0;
+  while(queue.length) {
+    const selected = queue.shift();
+    count++;
+    const toEnQueue = map[selected]; //所有的后续课
+    if(toEnQueue && toEnQueue.length) {
+      for(let i=0;i<toEnQueue.length;i++){
+        inDegree[toEnQueue[i]]--; //后续课上过了 入度减1
+        if(inDegree[toEnQueue[i]]==0){
+          queue.push(toEnQueue[i]);
+        }
+      }
+    }
+  }
+  return count == numCourses;
+
+};
+
+console.log(canFinish(6,
+  [[3, 0], [3, 1], [4, 1], [4, 2], [5, 3], [5, 4]]))
---
+++ 11-33. 搜索旋转排序数组.js
@@ -0,0 +1,29 @@
+/**
+ * 二分法
+ * https://leetcode.cn/problems/search-in-rotated-sorted-array/solutions/996781/cong-ji-ben-de-er-fen-fa-shuo-qi-ru-he-a-hof5/
+ */
+var search = function(nums, target) {
+  let left=0;
+  let right = nums.length -1;
+  while(left <= right) {
+    let mid =Math.floor((left+right)/2);
+    if(nums[mid] == target) {
+      return mid;
+    }
+    if(nums[left] <= nums[mid]) { //有等于的可能性
+      if(target < nums[left] || target > nums[mid] ){
+        left = mid+1;
+      }
+      else {
+        right = mid-1
+      }
+    }else {
+      if(target>nums[right] || target <nums[mid]) {
+        right = mid-1;
+      }else {
+        left = mid+1;
+      }
+    }
+  }
+  return -1
+};
